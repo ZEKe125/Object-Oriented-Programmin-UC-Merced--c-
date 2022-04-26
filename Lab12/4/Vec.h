c025168582c31b70012c5d83e150a2eabfa691d5
ZEKe125 <82134360+ZEKe125@users.noreply.github.com>
2022-04-26 12:27:06 -0700

    add lab12 skel

---
+++ Lab12/4/Vec.h
@@ -0,0 +1,33 @@
+#ifndef LA12_Vec_h
+#define LA12_Vec_h
+
+using namespace std;
+
+class Vec {
+public:
+    float x;
+    float y;
+
+    Vec(){
+        x = 0;
+        y = 0;
+    }
+    
+    Vec (float x, float y){
+        this->x = x;
+        this->y = y;
+    }
+    
+    Vec operator + (const Vec & v) const{
+        return Vec(x + v.x, y + v.y);
+    }
+    
+
+};
+
+ostream& operator<< (ostream& o, const Vec & v){
+    o << "(" << v.x << ", " << v.y << ")";
+    return o;
+}
+
+#endif
