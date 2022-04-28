3e91eda6fa8da51dd1248cc9285b18255e5cdae5
ZEKe125 <82134360+ZEKe125@users.noreply.github.com>
2022-04-27 20:42:02 -0700

    Update Mat.h

--- Lab12/5/Mat.h
+++ Lab12/5/Mat.h
@@ -10,45 +10,80 @@ class Mat
 {
 
 public:
-    float topLeft;
-    float bottomLeft;
-    float topRight;
-    float bottomRight;
+    //|A, B|
+    //|C, D|
+
+    float A;
+    float C;
+    float B;
+    float D;
 
     static Mat Identity;
 
-    Mat(float tl, float bl, float tr, float br)
+    Mat(float A, float B, float C, float D)
     {
-        this->topLeft = tl;
-        this->bottomLeft = bl;
-        this->topRight = tr;
-        this->bottomRight = br;
+        this->A = A;
+        this->B = B;
+        this->C = C;
+        this->D = D;
     };
 
     Mat()
     {
-        this->topLeft = 0.0;
-        this->bottomLeft = 0.0;
-        this->topRight = 0.0;
-        this->bottomRight = 0.0;
+        this->A = 0.0;
+        this->B = 0.0;
+        this->C = 0.0;
+        this->D = 0.0;
+    };
+
+    Mat operator-(const Mat &m) const
+    {
+        return Mat(A - m.A, B - m.B, C - m.C, D - m.D);
     };
 
     Mat operator+(const Mat &m) const
     {
-        return Mat(topLeft + m.topLeft, bottomLeft + m.bottomLeft, topRight + m.topRight, bottomRight + m.bottomRight);
+        return Mat(A + m.A, B + m.B, C + m.C, D + m.D);
     };
-    // Mat operator*(const int &scalar) const
+
+    // Mat operator*(const int &scalar)
     // {
-    //     return Mat(topLeft * scalar, bottomLeft * scalar, topRight * scalar, bottomRight * scalar);
+    //     return Mat(A * scalar,
+    //                B * scalar,
+    //                C * scalar,
+    //                D * scalar);
     // };
-
-    Mat operator*(const Mat &m) const {
-
-    };
 
     ~Mat(){};
 };
 
+// hard coded 2x2 identity matrix
 Mat Mat::Identity = Mat(1.0, 0.0, 0.0, 1.0);
+
+// * overload for Mat and Vec
+Vec operator*(const Vec &v, const Mat &m)
+{
+    // [2x1] * [2x2] multipilcation
+    return Vec(v.x * m.A + v.y * m.C, v.x * m.B + v.y * m.D);
+}
+
+// * overload for Mat and int
+Mat operator*(const int &scalar, const Mat &m)
+{
+    return Mat(m.A * scalar,
+               m.B * scalar,
+               m.C * scalar,
+               m.D * scalar);
+};
+
+// * overload for Mat and Mat
+Mat operator*(const Mat &f, const Mat &s)
+{
+    // 2x2 matrix multiplication
+    return Mat(f.A * s.A + f.B * s.C,
+               f.A * s.B + f.B * s.D,
+               f.C * s.A + f.D * s.C,
+               f.C * s.B + f.D * s.D);
+};
 
 #endif
